cdd693a481740ff6978c64e506a5335447ec3b8d
temporaltime93 <temporaltime93@gmail.com>
2025-07-14 10:14:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-
+from discord import app_commands
 import os
 
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -13,5 +13,13 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 # todo: Cargar comandos externos desde comandos.py
 import comandos_bot
 comandos_bot.setup(bot)
+
+@bot.event
+async def on_ready():
+    try:
+        synced = await bot.tree.sync()
+        print(f"✅ Comandos slash sincronizados ({len(synced)})")
+    except Exception as e:
+        print(f"❌ Error al sincronizar comandos slash: {e}")
 
 bot.run(TOKEN)
